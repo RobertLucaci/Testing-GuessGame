a1cb4962cb5b7d378621f53bfba9b03572888ca3
RobertLucaci <70859278+RobertLucaci@users.noreply.github.com>
2020-12-30 04:46:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import unittest
-import main
+import guessgame
 
 class GuessGame(unittest.TestCase):
     def test_input(self):
-        result = main.guess_game(5, 5)
+        result = guessgame.guess_game(5, 5)
         self.assertTrue(result)
 
     def test_input_wrong_guess(self):
-        result = main.guess_game(5, 0)
+        result = guessgame.guess_game(5, 0)
         self.assertFalse(result)
 
     def test_input_wrong_number(self):
-        result = main.guess_game(5, 25)
+        result = guessgame.guess_game(5, 25)
         self.assertFalse(result)
 
     def test_input_wrong_type(self):
-        result = main.guess_game(5, '25')
+        result = guessgame.guess_game(5, '25')
         self.assertFalse(result)
 
 if __name__ == '__main__':
